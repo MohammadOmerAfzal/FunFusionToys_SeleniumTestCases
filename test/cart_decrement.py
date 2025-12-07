9ad3874dec6012c5cbbc71420d2cb0eff4ea3f6e
Mohammad Omer Afzal <m.omarafzal12@gmail.com>
2025-12-07 10:03:21 +0500

    Update cart_decrement.py

--- test/cart_decrement.py
+++ test/cart_decrement.py
@@ -27,6 +27,7 @@ chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--remote-allow-origins=*")
 
 driver = webdriver.Remote(
     # UPDATED: Use the dynamic SELENIUM_URL
