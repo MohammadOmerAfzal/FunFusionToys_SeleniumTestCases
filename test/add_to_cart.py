8f681e5c0b7e0a55850a1f759f734077f84e5894
Mohammad Omer Afzal <m.omarafzal12@gmail.com>
2025-12-07 10:35:15 +0500

    Update add_to_cart.py

--- test/add_to_cart.py
+++ test/add_to_cart.py
@@ -23,6 +23,7 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--remote-allow-origins=*")
+chrome_options.add_argument("--disable-features=VizDisplayCompositor")
 
 # --- 3. Initialize Remote WebDriver ---
 driver = webdriver.Remote(
