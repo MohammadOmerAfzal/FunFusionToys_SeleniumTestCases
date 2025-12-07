2e4f0ace4c271939da3fad2a66ef52d0301ab629
Mohammad Omer Afzal <m.omarafzal12@gmail.com>
2025-12-07 10:05:06 +0500

    Update test_cart_quantity.py

--- test/test_cart_quantity.py
+++ test/test_cart_quantity.py
@@ -21,6 +21,8 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--remote-allow-origins=*") 
+    
 
 driver = webdriver.Remote(
     # UPDATED: Use the dynamic SELENIUM_URL
