14652b013b0d7cb72db6cd07514f7062ef92892e
Mohammad Omer Afzal <m.omarafzal12@gmail.com>
2025-12-07 10:04:35 +0500

    Update productsPage.py

--- test/productsPage.py
+++ test/productsPage.py
@@ -21,6 +21,8 @@ def test_real_product_count():
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--remote-allow-origins=*") 
+    options.add_argument("--disable-gpu") 
 
     driver = webdriver.Remote(
         # UPDATED: Use the dynamic SELENIUM_URL
