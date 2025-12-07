09050b092e3a6de3a73636f49393a3236841b099
Mohammad Omer Afzal <m.omarafzal12@gmail.com>
2025-12-07 10:03:33 +0500

    Update checkout.py

--- test/checkout.py
+++ test/checkout.py
@@ -19,6 +19,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--remote-allow-origins=*")
 
 driver = webdriver.Remote(
     # UPDATED: Use the dynamic SELENIUM_URL
