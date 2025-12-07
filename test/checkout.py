e3a3e46e7335522f99c668c7b16c088e33c12eb0
Mohammad Omer Afzal <m.omarafzal12@gmail.com>
2025-12-07 10:35:52 +0500

    Update checkout.py

--- test/checkout.py
+++ test/checkout.py
@@ -20,6 +20,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--remote-allow-origins=*")
+chrome_options.add_argument("--disable-features=VizDisplayCompositor")
 
 driver = webdriver.Remote(
     # UPDATED: Use the dynamic SELENIUM_URL
