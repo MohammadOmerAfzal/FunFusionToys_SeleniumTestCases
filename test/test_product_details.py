71dffb2010fedcad3bd89ebfd89287411fc6c54e
Mohammad Omer Afzal <m.omarafzal12@gmail.com>
2025-12-07 10:36:59 +0500

    Update test_product_details.py

--- test/test_product_details.py
+++ test/test_product_details.py
@@ -23,6 +23,7 @@ def test_product_details_page():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-allow-origins=*")
+    options.add_argument("--disable-features=VizDisplayCompositor")
 
     driver = webdriver.Remote(
         # UPDATED: Use the dynamic SELENIUM_URL
